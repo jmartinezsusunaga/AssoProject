99077ebd8c5c208f1768bd3e19a9c6b66030890d
jmartinezsusunaga <jonathan.martinez-susunaga@apside-groupe.com>
2023-10-19 10:04:31 +0200

    J'ai pris le pom fait avec Thomas

--- testPersonalProjectBDD/src/main/java/com/example/demo/DemoApplication.java
+++ testPersonalProjectBDD/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.testPersonalProject;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestPersonalProjectApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestPersonalProjectApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- testPersonalProjectBDD/src/main/java/com/example/testPersonalProject/entity/UserEntity.java
+++
@@ -1,5 +0,0 @@
-package com.example.testPersonalProject.entity;
-
-public class UserEntity {
-
-}
