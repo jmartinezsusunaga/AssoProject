117ced251b7b9ebd8b901054b6b4da6025220c8c
jmartinezsusunaga <dub.jona@gmail.com>
2023-12-07 17:03:15 +0100

    Modif entitiy

--- testPersonalProjectBDD/src/main/java/entity/EvenementsEntityDTO.java
+++ testPersonalProjectBDD/src/main/java/entity/EvenementsEntityDTO.java
@@ -7,41 +7,106 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+import java.sql.Date;
+
 @Entity
-@Table(name =  "test_2", schema = "public")
+@Table(name =  "evenements", schema = "public")
 public class EvenementsEntityDTO {
 
 	@Id
-	private Integer id_evenement_mre;
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id_ev;
 	
-	@Column
-	private String membre;
+	@Column(nullable = false, length = 100)
+	private String nom_ev;
+	
+	@Column(nullable = false, unique = true, length = 100)
+	private String lieu_ev;
+	
+	@Column(nullable = false, unique = true)
+	private Date date_ev;
+	
+	@Column(nullable = false)
+	private Integer capacite_event_ev;
+	
+	@Column(length = 1000)
+	private String visuel_facebook_ev;
+	
+	@Column(length = 1000)
+	private String visuel_insta_poste_ev;
+	
+	@Column(length = 1000)
+	private String visuel_insta_story_ev;
 
-	public EvenementsEntityDTO(Integer id_evenement_mre, String membre) {
+	public EvenementsEntityDTO(Integer id_evenement_mre) {
 		super();
-		this.id_evenement_mre = id_evenement_mre;
-		this.membre = membre;
+		this.id_ev = id_evenement_mre;
 	}
 	
 	public EvenementsEntityDTO() {
 		
 	}
 
-	public Integer getId_evenement_mre() {
-		return id_evenement_mre;
+	public Integer getId_ev() {
+		return id_ev;
 	}
 
-	public void setId_evenement_mre(Integer id_evenement_mre) {
-		this.id_evenement_mre = id_evenement_mre;
+	public void setId_ev(Integer id_ev) {
+		this.id_ev = id_ev;
 	}
 
-	public String getMembre() {
-		return membre;
+	public String getNom_ev() {
+		return nom_ev; 
 	}
 
-	public void setMembre(String membre) {
-		this.membre = membre;
+	public String getLieu_ev() {
+		return lieu_ev;
 	}
+
+	public void setLieu_ev(String lieu_ev) {
+		this.lieu_ev = lieu_ev;
+	}
+
+	public Date getDate_ev() {
+		return date_ev;
+	}
+
+	public void setDate_ev(Date date_ev) {
+		this.date_ev = date_ev;
+	}
+
+	public Integer getCapacite_event_ev() {
+		return capacite_event_ev;
+	}
+
+	public void setCapacite_event_ev(Integer capacite_event_ev) {
+		this.capacite_event_ev = capacite_event_ev;
+	}
+
+	public String getVisuel_facebook_ev() {
+		return visuel_facebook_ev;
+	}
+
+	public void setVisuel_facebook_ev(String visuel_facebook_ev) {
+		this.visuel_facebook_ev = visuel_facebook_ev;
+	}
+
+	public String getVisuel_insta_poste_ev() {
+		return visuel_insta_poste_ev;
+	}
+
+	public void setVisuel_insta_poste_ev(String visuel_insta_poste_ev) {
+		this.visuel_insta_poste_ev = visuel_insta_poste_ev;
+	}
+
+	public String getVisuel_insta_story_ev() {
+		return visuel_insta_story_ev;
+	}
+
+	public void setVisuel_insta_story_ev(String visuel_insta_story_ev) {
+		this.visuel_insta_story_ev = visuel_insta_story_ev;
+	}
+	
 	
 	
 }
--- testPersonalProjectBDD/src/main/java/entity/UtilisateurEntityDTO.java
+++ testPersonalProjectBDD/src/main/java/entity/UtilisateurEntityDTO.java
@@ -52,52 +52,170 @@ public class UtilisateurEntityDTO {
 	private String bio_art;
 	
 	@Column(length = 1000)
-	private String instagram_art;
+	private String lien_instagram_art;
 	
 	@Column(length = 1000)
-	private String soundCloud_art;
-
+	private String lien_soundCloud_art;
 	
+	@Column(length = 1000)
+	private String lien_youtube_art;
 	
+	@Column(length = 1000)
+	private String logo_ass_art;
 	
+	UtilisateurEntityDTO(Integer id_uti){
+			super();
+			this.id_uti = id_uti;
+		}
 	
-	
-	
-	
-	public UtilisateurEntityDTO(Integer id_test, String tes_name_t, Date test_date_t) {
-		this.id_test = id_test;
-		this.tes_name_t = tes_name_t;
-		this.test_date_t = test_date_t;
-	}
-	
-	public UtilisateurEntityDTO() {
-	}
-
-	public Integer getId_test() {
-		return id_test;
+	UtilisateurEntityDTO(){
+		
 	}
 
-	public void setId_test(Integer id_test) {
-		this.id_test = id_test;
+	public Integer getId_uti() {
+		return id_uti;
 	}
 
-	public String getTes_name_t() {
-		return tes_name_t;
+	public void setId_uti(Integer id_uti) {
+		this.id_uti = id_uti;
 	}
 
-	public void setTes_name_t(String tes_name_t) {
-		this.tes_name_t = tes_name_t;
+	public Date getDate_naissance_uti() {
+		return date_naissance_uti;
 	}
 
-	public Date getTest_date_t() {
-		return test_date_t;
+	public void setDate_naissance_uti(Date date_naissance_uti) {
+		this.date_naissance_uti = date_naissance_uti;
 	}
 
-	public void setTest_date_t(Date test_date_t) {
-		this.test_date_t = test_date_t;
+	public String getMdp_uti() {
+		return mdp_uti;
 	}
-	
-	
+
+	public void setMdp_uti(String mdp_uti) {
+		this.mdp_uti = mdp_uti;
+	}
+
+	public String getNom_uti() {
+		return nom_uti;
+	}
+
+	public void setNom_uti(String nom_uti) {
+		this.nom_uti = nom_uti;
+	}
+
+	public String getPrenom_uti() {
+		return prenom_uti;
+	}
+
+	public void setPrenom_uti(String prenom_uti) {
+		this.prenom_uti = prenom_uti;
+	}
+
+	public String getMail_uti() {
+		return mail_uti;
+	}
+
+	public void setMail_uti(String mail_uti) {
+		this.mail_uti = mail_uti;
+	}
+
+	public String getDroits_uti() {
+		return droits_uti;
+	}
+
+	public void setDroits_uti(String droits_uti) {
+		this.droits_uti = droits_uti;
+	}
+
+	public String getPersonnalite_juridique_uti() {
+		return personnalite_juridique_uti;
+	}
+
+	public void setPersonnalite_juridique_uti(String personnalite_juridique_uti) {
+		this.personnalite_juridique_uti = personnalite_juridique_uti;
+	}
+
+	public String getNom_artistique_uti() {
+		return nom_artistique_uti;
+	}
+
+	public void setNom_artistique_uti(String nom_artistique_uti) {
+		this.nom_artistique_uti = nom_artistique_uti;
+	}
+
+	public String getPhoto_uti() {
+		return photo_uti;
+	}
+
+	public void setPhoto_uti(String photo_uti) {
+		this.photo_uti = photo_uti;
+	}
+
+	public String getType_poste_association_uti() {
+		return type_poste_association_uti;
+	}
+
+	public void setType_poste_association_uti(String type_poste_association_uti) {
+		this.type_poste_association_uti = type_poste_association_uti;
+	}
+
+	public String getBio_art() {
+		return bio_art;
+	}
+
+	public void setBio_art(String bio_art) {
+		this.bio_art = bio_art;
+	}
+
+	public String getInstagram_art() {
+		return lien_instagram_art;
+	}
+
+	public void setInstagram_art(String instagram_art) {
+		this.lien_instagram_art = instagram_art;
+	}
+
+	public String getSoundCloud_art() {
+		return lien_soundCloud_art;
+	}
+
+	public void setSoundCloud_art(String soundCloud_art) {
+		this.lien_soundCloud_art = soundCloud_art;
+	}
+
+	public String getLien_instagram_art() {
+		return lien_instagram_art;
+	}
+
+	public void setLien_instagram_art(String lien_instagram_art) {
+		this.lien_instagram_art = lien_instagram_art;
+	}
+
+	public String getLien_soundCloud_art() {
+		return lien_soundCloud_art;
+	}
+
+	public void setLien_soundCloud_art(String lien_soundCloud_art) {
+		this.lien_soundCloud_art = lien_soundCloud_art;
+	}
+
+	public String getLien_youtube_art() {
+		return lien_youtube_art;
+	}
+
+	public void setLien_youtube_art(String lien_youtube_art) {
+		this.lien_youtube_art = lien_youtube_art;
+	}
+
+	public String getLogo_ass_art() {
+		return logo_ass_art;
+	}
+
+	public void setLogo_ass_art(String logo_ass_art) {
+		this.logo_ass_art = logo_ass_art;
+	}
+
 	
 	
 }
